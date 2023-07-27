77dd80f846d0e53664db3c6fe431b91ea10de944
Alex <alextrnnn@gmail.com>
2023-07-26 21:51:00 -0600

    Fixed type error with ObjectID

--- dao/reviewsDAO.js
+++ dao/reviewsDAO.js
@@ -1,5 +1,5 @@
 import mongodb from "mongodb"
-const ObjectId = mongodb.ObjectID
+import { ObjectId } from "mongodb"
 
 let reviews
 
@@ -32,7 +32,8 @@ export default class ReviewsDAO {
 
     static async getReview(reviewId) {
         try {
-            return await reviews.findOne({ _id: ObjectId(reviewId) })
+            console.log(reviewId)
+            return await reviews.findOne({ _id: new ObjectId(reviewId) })
         } catch (e) {
             console.error(`Unable to get review: ${e}`)
             return { error: e }
